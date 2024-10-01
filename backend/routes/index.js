da396af884b7e7f742f3f95c191822a0cdc7cabc
sneh <patel.sneh2102@gmail.com>
2024-10-01 12:52:16 -0300

    feat: Added Main v1 Routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,18 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const authenticationRouter = require('./authenticationRouter'); 
-
+const authenticate = require('../middleware/middleware');
 
 const router = Router();
 
+router.get("/me", authenticate, async (req, res) => {
+    try {
+        const user = await User.findById(req.id); // Ensure you're using await for async operations
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 router.use("/v1", authenticationRouter);
+
+module.exports = router;
