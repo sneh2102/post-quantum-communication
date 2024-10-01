a4ea7aab22412e164d5a44551942cec103413876
sneh <patel.sneh2102@gmail.com>
2024-10-01 12:55:39 -0300

    Feat: Added Authentication Routes

--- backend/routes/authenticationRouter.js
+++ backend/routes/authenticationRouter.js
@@ -1,2 +1,18 @@
-const router = express.Router();
+const { Router } = require('express');
+const { register, login, confirmation, logout } = require('../controller/authentication');
+
+// Initialize router instance
+const router = Router();
+
+// Define your routes
+router.post('/register', register);
+router.post('/login', login);
+router.get('/confirmation/:token', confirmation);
+router.get('/logout',logout);
+
+// Export the router
+module.exports = router;
+
+
+
 
